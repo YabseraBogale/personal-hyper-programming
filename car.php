a72befd3d440ce4b0927a4261f34fe0cf7d6293d
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-07-02 18:36:52 +0300

    Add files via upload

---
+++ car.php
@@ -0,0 +1,16 @@
+
+
+<html>
+	<?php 
+$carname=$_POST['carname'];
+$price=$_POST['price'];
+$location=$_POST['location'];
+?>
+	<head>
+		<title>Car info</title>
+		</head>
+	<body>
+		<p>Carname: <?php echo $carname;?></p>
+		<p>Price: <?php echo $price;?>
+		</body>
+	</html>
