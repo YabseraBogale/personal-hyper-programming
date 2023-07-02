51554e26b1715739fb4a82112045b85a1d1a79cd
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-07-02 18:37:17 +0300

    Add files via upload

---
+++ carform.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>Car Form</title>
+	</head>
+	<body>
+		<form method="post" action="car.php">
+			<p>Car Name <input type="text" name="carname"></p>
+			<p>location <input  type="text" name="location"></p>
+			<p>Price <input type="number" name="price" ></p>
+			<input type="submit" value="submit">
+		</form>
+		</body>
+	</html>
