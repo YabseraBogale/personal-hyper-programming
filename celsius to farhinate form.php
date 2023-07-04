25836703ed2f2e470c9a3c7c5e619413e4064c48
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-07-04 19:22:51 +0300

    Add files via upload

---
+++ celsius to farhinate form.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>converts it to the Fahrenheit</title>
+	</head>
+	<body>
+		<form method="post" action="celsius to farhinate.php">
+			<p>Enter celsius <input type="number" name="celsius"></p>
+			<input type="submit" value="submit">
+		</form>
+	</body>
+	</html>
